25b3a9c02b74b2149f9a152bc15da78b115c941a
Savitha-E <savithaanjali.chi@gmail.com>
2026-02-14 16:32:24 +0530

    Removed folders

--- Lab1/6.py
+++
@@ -1,4 +0,0 @@
-#Implement y = 2x1 + 3x2 + 3x3 + 4, where x1, x2 and x3 are three independent variables.
-# Compute the gradient of y and print the values.
-
-x=
--- Lab1/trial.py
+++
@@ -1,4 +0,0 @@
-x=4
-import math
-x2=math.sqrt(x)
-print(x2)
